039510e45583076585ca1c93a134cf6600c0a5c0
jonathan <20475437+JHeinzde@users.noreply.github.com>
2026-06-08 23:00:14 +0200

    Add micro-aes template to the repository
    This ensures that all of the parts required for the emulator exercise
    are in one place and students dont have to look in many places.

---
+++ micro-aes-template/main.c
@@ -0,0 +1,9 @@
+#include "cpuemu.h"
+#include "micro_aes.c" // We need to include the C file, because we are only going to the assembly stage and are not linking.
+#include "micro_aes.h"
+#include <stdint.h>
+
+
+int main() {
+  return 0; 
+}
